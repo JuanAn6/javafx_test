98ea0b6067a7578f336c90b32562d8e54436ea03
JuanAn6 <juanantoniorizija666@gmail.com>
2026-02-26 00:05:51 +0100

    Lateral menu and toggle with an option in the top menu

--- src/main/java/com/manager/controller/MainController.java
+++ src/main/java/com/manager/controller/MainController.java
@@ -3,7 +3,11 @@ package com.manager.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
@@ -11,10 +15,36 @@ public class MainController {
 
     @FXML
     private StackPane contentPane;
+    
+    @FXML
+    private ListView<String> menuList;
+
+    @FXML
+    private SplitPane splitPane;
+
+    @FXML
+    private VBox sideMenu;
+
+    @FXML
+    private CheckMenuItem toggleSideMenu;
 
     @FXML
     public void initialize() {
         loadView("home.fxml"); // vista inicial
+
+        menuList.getItems().addAll(
+            "Opción 1",
+            "Opción 2",
+            "Opción 3",
+            "Opción 4",
+            "Opción 5",
+            "Opción 6",
+            "Opción 7",
+            "Opción 8",
+            "Opción 9",
+            "Opción 10"
+        );
+
     }
 
     @FXML
@@ -37,4 +67,19 @@ public class MainController {
             e.printStackTrace();
         }
     }
+
+    @FXML
+    private void toggleSideMenu() {
+
+        if (toggleSideMenu.isSelected()) {
+            if (!splitPane.getItems().contains(sideMenu)) {
+                splitPane.getItems().add(0, sideMenu);
+                splitPane.setDividerPositions(0.1);
+            }
+        } else {
+            splitPane.getItems().remove(sideMenu);
+        }
+    }
+
+
 }
